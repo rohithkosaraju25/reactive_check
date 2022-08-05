09b8be74f720b89b570e2af0d3b965fd29b9c47c
rohithkosaraju25 <rohith.kosaraju25@gmail.com>
2022-08-05 23:37:01 +0530

    initial commit

---
+++ src/main/java/com/reactive/progrmming/reactive_check/ReactiveCheckApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.progrmming.reactive_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveCheckApplication.class, args);
+	}
+
+}
